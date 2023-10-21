848a850c4f9b3b4302c5d31617bef8a0e342d4f0
Kows-alya <148610633+Kows-alya@users.noreply.github.com>
2023-10-21 12:51:21 +0530

    Add files via upload

--- formproject/formapp/views.py
+++ formproject/formapp/views.py
@@ -5,13 +5,25 @@ def form_name_view(request):
     form = forms.FormName()
 # check to see if we are getting a POST request back    
     if request.method == 'POST':
-        form=forms.FormName(request.POST)
-        
-# Then we check to see if the form is valid (This is automatic validation by Django)        
-        if form.is_valid():
-# if form.is_valid == True then  do something            
-            print("Form validation successful! See console for information:")
-            print("Name : "+forms.cleaned_data['name'])
-            print("Email : "+forms.cleaned_data['email'])
-            print("Message : "+forms.cleaned_data['text'])
+        form = forms.FormName(request.POST)       
+        if form.is_valid():       
+         print("Form validation successful! See console for information:")
+         print("Name : "+form.cleaned_data['name'])
+         print("Email : "+form.cleaned_data['email'])
+         print("Message : "+form.cleaned_data['text'])
     return render(request,'home.html',{'form':form})
+
+# from django.shortcuts import render
+# from . import forms
+
+# def form_name_view(request):
+#     form = forms.FormName()
+# #Check to see if we are getting a POST request back
+#     if request.method == "POST":
+#         form = forms.FormName(request.POST)
+#         if form.is_valid():
+#          print("Form validation successful! See console for information:")
+#          print("Name: "+form.cleaned_data['name'])
+#          print("email: "+form.cleaned_data['email'])
+#          print("message: "+form.cleaned_data['text'])
+#     return render(request, 'home.html', {'form': form})
